3346b72c1eeb5b75ff9b1c0b0baa81664c881253
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2023-04-06 22:23:25 +0300

    add solution

---
+++ lesson3_combine_reducers/task2_dev-tools/src/index.js
@@ -0,0 +1,9 @@
+import store from './counter';
+import { increment, decrement } from './counter';
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
